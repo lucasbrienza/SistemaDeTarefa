a92f66a83c544f3ec6d3ec97824a7463c6c629a4
lucas <lucas@LAPTOP-219JAHLV>
2023-12-15 20:42:26 -0300

    Adicionar arquivos de projeto.

---
+++ Data/SistemaTarefasDBContex.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefa.Data.Map;
+using SistemaDeTarefa.Models;
+
+namespace SistemaDeTarefa.Data
+{
+    public class SistemaTarefasDBContex : DbContext
+    {
+        public SistemaTarefasDBContex(DbContextOptions<SistemaTarefasDBContex> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+        public DbSet<TarefaModel> Tarefas { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UsuarioMap());
+            modelBuilder.ApplyConfiguration(new TarefaMap());
+            base.OnModelCreating(modelBuilder);
+        }
+        
+    }
+}
---
+++ Models/TarefaModel.cs
@@ -0,0 +1,12 @@
+﻿using SistemaDeTarefa.Enums;
+
+namespace SistemaDeTarefa.Models
+{
+    public class TarefaModel
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public StatusTarefa Status { get; set; }
+    }
+}
